4f3a556de76295e0c30632a021a30440f2ad9c6d
sulochanaakki <akki.sulochana@gmail.com>
2022-11-09 21:30:15 +0100

    model training,model evalution and model pusher jobs done

--- main.py
+++ main.py
@@ -4,11 +4,36 @@ import os,sys
 from sensor.logger import logging
 from sensor.pipeline import training_pipeline
 from sensor.pipeline.training_pipeline import TrainPipeline
+from sensor.utils.main_utils import read_yaml_file
+
+env_file_path=os.path.join(os.getcwd(),"env.yaml")
+
+def set_env_variable(env_file_path):
+
+    if os.getenv('MONGO_DB_URL',None) is None:
+        env_config = read_yaml_file(env_file_path)
+        os.environ['MONGO_DB_URL']=env_config['MONGO_DB_URL']
 
 if __name__ == '__main__':
     try:
+        env_file_path="/config/workspace/env.yaml"
+        set_env_variable(env_file_path)
         training_pipeline = TrainPipeline()
         training_pipeline.run_pipeline()
     except Exception as e:
         print(e)
         logging.exception(e)
+
+
+def main():
+    try:
+        set_env_variable(env_file_path)
+        training_pipeline = TrainPipeline()
+        training_pipeline.run_pipeline()
+    except Exception as e:
+        print(e)
+        logging.exception(e)
+
+
+if __name__=="__main__":
+    main()
--- sensor/data_access/sensor_data.py
+++ sensor/data_access/sensor_data.py
@@ -3,7 +3,7 @@ from typing import Optional
 
 import numpy as np
 import pandas as pd
-
+import json
 from sensor.configuration.mongo_db_connection import MongoDBClient
 from sensor.constant.database import DATABASE_NAME
 from sensor.exception import SensorException
@@ -22,6 +22,22 @@ class SensorData:
 
         except Exception as e:
             raise SensorException(e, sys)
+
+
+    def save_csv_file(self,file_path ,collection_name: str, database_name: Optional[str] = None):
+        try:
+            data_frame=pd.read_csv(file_path)
+            data_frame.reset_index(drop=True, inplace=True)
+            records = list(json.loads(data_frame.T.to_json()).values())
+            if database_name is None:
+                collection = self.mongo_client.database[collection_name]
+            else:
+                collection = self.mongo_client[database_name][collection_name]
+            collection.insert_many(records)
+            return len(records)
+        except Exception as e:
+            raise SensorException(e, sys)
+
 
     def export_collection_as_dataframe(
         self, collection_name: str, database_name: Optional[str] = None) -> pd.DataFrame:
